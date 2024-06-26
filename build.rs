564832e4fedc998e25d9e3f572d9787bed24e434
Tarin Mahmood <34904+tmahmood@users.noreply.github.com>
2024-06-26 19:49:14 +0600

    Tries to run tailwindcss-cli that is available in the project folder

--- build.rs
+++ build.rs
@@ -2,11 +2,23 @@ use std::process::Command;
 
 fn main() {
     println!("cargo:rerun-if-changed=frontend/");
-    if !Command::new("tailwindcss")
+    let css_build_success = match Command::new("tailwindcss")
         .args(["-i", "frontend/css/style.css", "-o", "dist/style.css", "-c", "frontend/tailwind.config.js"])
-        .status().unwrap().success() {
-        panic!("Failed to process css")
-    }
+        .status() {
+        Ok(vv) => {
+            vv.success()
+        }
+        Err(e) => {
+            false
+        }
+    };
+    if !css_build_success {
+        if let Err(e) = Command::new("./tailwindcss")
+            .args(["-i", "frontend/css/style.css", "-o", "dist/style.css", "-c", "frontend/tailwind.config.js"])
+            .status() {
+            panic!("Failed to process css")
+        }
+    };
     if !Command::new("rollup")
         .args(["-c", "frontend/rollup.config.js"])
         .status().unwrap().success() {
