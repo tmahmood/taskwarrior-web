2948e9032890d8c590aabb3a0ff0055d7027878a
Tarin Mahmood <34904+tmahmood@users.noreply.github.com>
2025-02-26 00:31:45 +0600

    WIP

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "frontend/templates/*.html",
+        "frontend/src/*",
+        "frontend/src/*"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+    ],
+}
+
