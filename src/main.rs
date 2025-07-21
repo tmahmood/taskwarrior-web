c164788e1de1244df5785dbef4b2cfefbe112286
Dmitry V. Luciv <dluciv@dluciv.name>
2025-07-21 16:28:39 +0300

    IP to listen on

--- src/main.rs
+++ src/main.rs
@@ -42,7 +42,8 @@ async fn main() {
     };
 
     let addr = format!(
-        "0.0.0.0:{}",
+        "{}:{}",
+        env::var("TWK_SERVER_ADDR").unwrap_or("0.0.0.0".to_string())
         env::var("TWK_SERVER_PORT").unwrap_or("3000".to_string())
     );
 
